AICP/frameworks_base

core: move OmniJaws client to its new home
Change-Id: I50349a910aa604959def70377da135c6d94c7774

core/java/com/android/internal/util/aicp/OmniJawsClient.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.android.systemui.omni;
+package com.android.internal.util.aicp;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -41,8 +41,6 @@ import android.os.UserHandle;
 import android.preference.PreferenceActivity;
 import android.provider.Settings;
 import android.util.Log;
-
-import com.android.systemui.R;
 
 public class OmniJawsClient {
     private static final String TAG = "SystemUI:OmniJawsClient";

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -55,7 +55,7 @@ import androidx.slice.builders.ListBuilder;
 import androidx.slice.builders.ListBuilder.RowBuilder;
 import androidx.slice.builders.SliceAction;
 
-import com.android.systemui.omni.OmniJawsClient;
+import com.android.internal.util.aicp.OmniJawsClient;
 
 /**
  * Simple Slice provider that shows the current date.

packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
@@ -46,8 +46,8 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.aicp.OmniJawsClient;
 import com.android.systemui.R;
-import com.android.systemui.omni.OmniJawsClient;
 import com.android.systemui.statusbar.phone.SettingsButton;
 import com.android.systemui.plugins.ActivityStarter;
 

packages/SystemUI/src/com/android/systemui/qs/tiles/WeatherTile.java
@@ -41,11 +41,11 @@ import android.widget.Toast;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.aicp.OmniJawsClient;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.omni.DetailedWeatherView;
-import com.android.systemui.omni.OmniJawsClient;
 import com.android.systemui.plugins.qs.DetailAdapter;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.qs.QSDetailItems;

anuragbhaumik/frameworks_base-2

core/java/com/android/internal/util/omni/OmniJawsClient.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.android.systemui.omni;
+package com.android.internal.util.omni;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -40,8 +40,6 @@ import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
-
-import com.android.systemui.R;
 
 public class OmniJawsClient {
     private static final String TAG = "SystemUI:OmniJawsClient";

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardSliceProvider.java
@@ -51,13 +51,13 @@ import androidx.slice.builders.ListBuilder.RowBuilder;
 import androidx.slice.builders.SliceAction;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.omni.OmniJawsClient;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.SystemUIAppComponentFactory;
 import com.android.systemui.SystemUIFactory;
-import com.android.systemui.omni.OmniJawsClient;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.statusbar.NotificationMediaManager;
 import com.android.systemui.statusbar.StatusBarState;

packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
@@ -46,8 +46,8 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.omni.OmniJawsClient;
 import com.android.systemui.R;
-import com.android.systemui.omni.OmniJawsClient;
 import com.android.systemui.statusbar.phone.SettingsButton;
 import com.android.systemui.plugins.ActivityStarter;
 

packages/SystemUI/src/com/android/systemui/qs/tiles/WeatherTile.java
@@ -41,11 +41,11 @@ import android.widget.Toast;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.omni.OmniJawsClient;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.omni.DetailedWeatherView;
-import com.android.systemui.omni.OmniJawsClient;
 import com.android.systemui.plugins.qs.DetailAdapter;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.qs.QSDetailItems;