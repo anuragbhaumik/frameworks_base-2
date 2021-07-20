Octavi-OS/platform_frameworks_base

sFunyClock: Allow Accent color and improve layout
Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>
Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -74,6 +74,7 @@ public class SfunyClockController implements ClockPlugin {
      */
     private TextClock mHourClock;
     private TextClock mMinuteClock;
+    private TextClock mDate;
 
     private Context mContext;
 
@@ -127,12 +128,12 @@ public class SfunyClockController implements ClockPlugin {
         View previewView = mLayoutInflater.inflate(R.layout.digital_sfuny_preview, null);
         TextClock previewHourTime = previewView.findViewById(R.id.clockHour);
         TextClock previewMinuteTime = previewView.findViewById(R.id.clockMinute);
-        TextClock previewDate = previewView.findViewById(R.id.date);
+        //TextClock previewDate = previewView.findViewById(R.id.date);
 
         // Initialize state of plugin before generating preview.
         previewHourTime.setTextColor(Color.WHITE);
         previewMinuteTime.setTextColor(Color.WHITE);
-        previewDate.setTextColor(Color.WHITE);
+        //previewDate.setTextColor(Color.WHITE);
         ColorExtractor.GradientColors colors = mColorExtractor.getColors(
                 WallpaperManager.FLAG_LOCK);
         setColorPalette(colors.supportsDarkText(), colors.getColorPalette());
@@ -164,13 +165,17 @@ public class SfunyClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        if(Utils.useLockscreenClockAccentColor(mContext)) {
-            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
-            mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        mMinuteClock.setTextColor(Color.WHITE);
+        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+            mHourClock.setTextColor((mContext.getResources().getColor(R.color.lockscreen_clock_accent_color)));
         } else {
             mHourClock.setTextColor(color);
-            mMinuteClock.setTextColor(color);
         }
+        // if(ZenxUtils.useLockscreenClockAccentColor(mContext)) {
+        //     mDate.setTextColor(color);
+        // } else {
+        //     mDate.setTextColor(color);
+        // }
     }
 
     @Override

anuragbhaumik/frameworks_base-2

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -75,6 +75,7 @@ public class SfunyClockController implements ClockPlugin {
      */
     private TextClock mHourClock;
     private TextClock mMinuteClock;
+    private TextClock mDate;
 
     private Context mContext;
 
@@ -128,12 +129,12 @@ public class SfunyClockController implements ClockPlugin {
         View previewView = mLayoutInflater.inflate(R.layout.digital_sfuny_preview, null);
         TextClock previewHourTime = previewView.findViewById(R.id.clockHour);
         TextClock previewMinuteTime = previewView.findViewById(R.id.clockMinute);
-        TextClock previewDate = previewView.findViewById(R.id.date);
+        //TextClock previewDate = previewView.findViewById(R.id.date);
 
         // Initialize state of plugin before generating preview.
         previewHourTime.setTextColor(Color.WHITE);
         previewMinuteTime.setTextColor(Color.WHITE);
-        previewDate.setTextColor(Color.WHITE);
+        //previewDate.setTextColor(Color.WHITE);
         ColorExtractor.GradientColors colors = mColorExtractor.getColors(
                 WallpaperManager.FLAG_LOCK);
         setColorPalette(colors.supportsDarkText(), colors.getColorPalette());
@@ -165,13 +166,17 @@ public class SfunyClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        if(Utils.useLockscreenClockAccentColor(mContext)) {
-            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
-            mAnalogClock.setClockColors(primary, mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        mMinuteClock.setTextColor(Color.WHITE);
+        if(derpUtils.useLockscreenClockAccentColor(mContext)) {
+            mHourClock.setTextColor((mContext.getResources().getColor(R.color.lockscreen_clock_accent_color)));
         } else {
             mHourClock.setTextColor(color);
-            mMinuteClock.setTextColor(color);
         }
+        // if(derpUtils.useLockscreenClockAccentColor(mContext)) {
+        //     mDate.setTextColor(color);
+        // } else {
+        //     mDate.setTextColor(color);
+        // }
     }
 
     @Override