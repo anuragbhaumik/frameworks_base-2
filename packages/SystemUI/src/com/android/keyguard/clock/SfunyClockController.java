Octavi-OS/platform_frameworks_base

Keyguard: Make clock widgets great again
@ZeNiXxX
* allow setting accent color for Minutes and hours on every digital clock
* also allow setting accent color for custom clocks

packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -166,16 +166,11 @@ public class SfunyClockController implements ClockPlugin {
     @Override
     public void setTextColor(int color) {
         mMinuteClock.setTextColor(Color.WHITE);
-        if(OctaviUtils.useLockscreenClockAccentColor(mContext)) {
+        if(OctaviUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mHourClock.setTextColor((mContext.getResources().getColor(R.color.lockscreen_clock_accent_color)));
         } else {
             mHourClock.setTextColor(color);
         }
-        // if(ZenxUtils.useLockscreenClockAccentColor(mContext)) {
-        //     mDate.setTextColor(color);
-        // } else {
-        //     mDate.setTextColor(color);
-        // }
     }
 
     @Override

anuragbhaumik/frameworks_base-2

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/SfunyClockController.java
@@ -167,16 +167,11 @@ public class SfunyClockController implements ClockPlugin {
     @Override
     public void setTextColor(int color) {
         mMinuteClock.setTextColor(Color.WHITE);
-        if(derpUtils.useLockscreenClockAccentColor(mContext)) {
+        if(derpUtils.useLockscreenCustomClockAccentColor(mContext)) {
             mHourClock.setTextColor((mContext.getResources().getColor(R.color.lockscreen_clock_accent_color)));
         } else {
             mHourClock.setTextColor(color);
         }
-        // if(derpUtils.useLockscreenClockAccentColor(mContext)) {
-        //     mDate.setTextColor(color);
-        // } else {
-        //     mDate.setTextColor(color);
-        // }
     }
 
     @Override