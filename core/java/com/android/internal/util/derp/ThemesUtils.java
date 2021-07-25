TenX-OS/frameworks_base

SystemUI: Introduce Custom Statusbar Signal & Wi-Fi icons [1/3]
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

core/java/com/android/internal/util/tenx/ThemesUtils.java
@@ -127,6 +127,20 @@ public class ThemesUtils {
         "com.android.system.switch.telegram", // 6
     };
 
+    // Statusbar Signal icons
+    private static final String[] SIGNAL_BAR = {
+        "com.tenx.systemui.signalbar_a",
+        "com.tenx.systemui.signalbar_b",
+        "com.tenx.systemui.signalbar_c",
+    };
+
+    // Statusbar Wifi icons
+    private static final String[] WIFI_BAR = {
+        "com.tenx.systemui.wifibar_a",
+        "com.tenx.systemui.wifibar_b",
+        "com.tenx.systemui.wifibar_c",
+    };
+
     public String[] getTheme(int theme) {
         switch (theme) {
             case DEVICE_THEME_LIGHT:

anuragbhaumik/frameworks_base-2

core/java/com/android/internal/util/derp/ThemesUtils.java
@@ -88,6 +88,20 @@ public static final String TAG = "ThemesUtils";
         "com.android.system.switch.fluid", // 7
     };
 
+    // Statusbar Signal icons
+    private static final String[] SIGNAL_BAR = {
+        "com.derp.systemui.signalbar_a",
+        "com.derp.systemui.signalbar_b",
+        "com.derp.systemui.signalbar_c",
+    };
+
+    // Statusbar Wifi icons
+    private static final String[] WIFI_BAR = {
+        "com.derp.systemui.wifibar_a",
+        "com.derp.systemui.wifibar_b",
+        "com.derp.systemui.wifibar_c",
+    };
+
     public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {
         if (switchStyle == 1) {
             stockSwitchStyle(om, userId);