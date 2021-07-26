d139e711848a3f6040b6219346f731d7c23dc8c9
JarlPenguin <jarlpenguin@outlook.com>
2021-07-26 12:41:18 +0530

    PixelPropsUtils: Spoof Pixel XL for Google Photos

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -29,7 +29,8 @@ public class PixelPropsUtils {
     private static final boolean DEBUG = false;
 
     private static final Map<String, Object> propsToChange;
-
+    private static final Map<String, Object> propsToChangePixelXL;
+    
     private static final String[] packagesToChange = {
             "com.google.android.ext.services",
             "com.google.android.apps.pixelmigrate",
@@ -56,12 +57,15 @@ public class PixelPropsUtils {
             "com.google.android.pixel.setupwizard",
             "com.google.android.apps.messaging",
             "com.google.android.apps.fitness",
-            "com.google.android.apps.photos",
             "com.google.android.apps.recorder",
             "com.google.android.apps.tachyon",
             "com.google.android.apps.maps",
             "com.google.pixel.dynamicwallpapers",
             "com.google.pixel.livewallpaper"
+    };
+    
+    private static final String[] packagesToChangePixelXL = {
+            "com.google.android.apps.photos"
     };
 
     static {
@@ -71,6 +75,13 @@ public class PixelPropsUtils {
         propsToChange.put("DEVICE", "redfin");
         propsToChange.put("PRODUCT", "redfin");
         propsToChange.put("MODEL", "Pixel 5");
+        propsToChangePixelXL = new HashMap<>();
+        propsToChangePixelXL.put("BRAND", "google");
+        propsToChangePixelXL.put("MANUFACTURER", "Google");
+        propsToChangePixelXL.put("DEVICE", "marlin");
+        propsToChangePixelXL.put("PRODUCT", "marlin");
+        propsToChangePixelXL.put("MODEL", "Pixel XL");
+        propsToChangePixelXL.put("FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys");
     }
 
     public static void setProps(String packageName) {
@@ -90,11 +101,24 @@ public class PixelPropsUtils {
                 setPropValue(key, value);
             }
         }
+        
+        if (Arrays.asList(packagesToChangePixelXL).contains(packageName)){
+            if (DEBUG){
+                Log.d(TAG, "Defining props for: " + packageName);
+            }
+            for (Map.Entry<String, Object> prop : propsToChangePixelXL.entrySet()) {
+                String key = prop.getKey();
+                Object value = prop.getValue();
+                setPropValue(key, value);
+            }
+        }
+                
         // Set proper indexing fingerprint
         if (packageName.equals("com.google.android.settings.intelligence")){
             setPropValue("FINGERPRINT", Build.DATE);
         }
-    }
+    }         
+    
 
     private static void setPropValue(String key, Object value){
         try {
